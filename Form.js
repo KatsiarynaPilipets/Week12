f1fb13b78a82fdd8c52a9d0a0a5a0de93547210e
kotenochek27 <katrinya1@gmail.com>
2023-05-29 11:43:00 -0400

    add files

--- Form.js
+++ Form.js
@@ -1,24 +1,18 @@
-let user = document.getElementsById("name");
-let avatar = document.getElementsById("link");
+let user = document.getElementById("name");
+let avatar = document.getElementById("link");
 let comment = document.querySelector(".comment");
 let chat = document.querySelector(".chat");
 
-user.addEventListener("input", function() {
-    capitalize(this);
-});
-
-function capitalize(user) {
-    user.value = user.value.replace(/(^|\s)\s/g, function (a) {
-        return a.toUpperCase();
-    });
-}
-
-function addNewMessage() {
-    chat.innerHTML = `<div class="container">
+function addNewMessage(e) {
+  console.log("work");
+  e.preventDefault();
+  let newUser =
+    user.value.charAt(0).toUpperCase() + user.value.slice(1).toLowerCase();
+  chat.innerHTML = `<div class="container">
     <img src="${avatar.value}" alt="avatar" class="avatar">
-    <p class="name">${user.value}</p>
-    <p class="comment">${comment.value.replace(/(xxx|viagra)/gi,"***")}</p>
+    <p class="name">${newUser}</p>
+    <p class="comment">${comment.value.replace(/(xxx|viagra)/gi, "***")}</p>
     </div>`;
 }
-
+let button = document.querySelector(".button");
 button.addEventListener("click", addNewMessage);
