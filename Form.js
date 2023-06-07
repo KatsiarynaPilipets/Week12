ae9443f72d34a66b011a7b0c423538f4b1bf0792
kotenochek27 <katrinya1@gmail.com>
2023-06-07 14:49:12 -0400

    add files

--- Form.js
+++ Form.js
@@ -2,30 +2,55 @@ const user = document.getElementById("name");
 const avatar = document.getElementById("link");
 const comment = document.querySelector(".comment");
 const chat = document.querySelector(".chat");
+const newName = document.querySelector(".username");
 
+
+const getDate = () => {
+  let data = new Date();
+  let date = `${data.toLocaleDateString()} ${data.toLocaleTimeString()}`;
+};
+
+const getNewName = () => {
+  let checkbox = document.querySelector('input[name="showName"]:checked').value;
+  if ((checkbox == "Yes") && (user != "")) {
+    let newUser =
+    user.value.charAt(0).toUpperCase() + user.value.slice(1).toLowerCase();
+  }
+  else if (checkbox == "No") {
+      newName.textContent = "Username";
+  };
+};
+
+const getNewImage = () => {
+  let img = [ "assets/1.png" , "assets//2.png" , "assets/3.png" , "assets/4.png" , "assets/5.png" , "assets/6.png"
+];
+let newImg = Math.round(Math.random() *  img.length);
+  if (yourFoto.value != "") {
+document.querySelector(".photo").src = photo;
+  }
+  else {
+    document.querySelector(".photo").src = img[newImg];
+  };
+};
 
 function addNewMessage(e) {
+  getDate();
+  getNewName ();
+  getNewImage ();
   e.preventDefault();
-  const formData = new FormData(form2);
-  const name = formData.get('name');
-    let url = formData.get('url');
+
+  let data = new Date();
+  let date = data.getDate();
   let newUser =
     user.value.charAt(0).toUpperCase() + user.value.slice(1).toLowerCase();
+
   chat.innerHTML = `<div class="container">
+    <p class="date">${data}</p>
     <img src="${avatar.value}" alt="avatar" class="avatar">
     <p class="name">${newUser}</p>
     <p class="comment">${comment.value.replace(/(xxx|viagra)/gi, "***")}</p>
     </div>`;
 }
 
-const getDate = () => {
-let data = new Date;
-  let date = date.getFullYear();
-
-  return date;
-}
-
-console.log(getDate);
-
 let button = document.querySelector(".button");
-button.addEventListener("click", addNewMessage, getDate);
+button.addEventListener("click", addNewMessage);
